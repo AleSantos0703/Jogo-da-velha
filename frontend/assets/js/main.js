acfa4a86d658fafba997ecd2cefd48709f4e354a
crezin <cremilsonyahoo78@gmail.com>
2026-04-13 21:07:54 -0300

    modificado por cremilson

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,13 +1,1 @@
 // Arquivo principal JavaScript do Frontend
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Frontend Projeto IFC carregado com sucesso!');
-    initFrontend();
-});
-function initFrontend() {
-    const container = document.querySelector('.container');
-    if (container) {
-        container.addEventListener('click', function() {
-            alert('Frontend funcionando!');
-        });
-    }
-}
