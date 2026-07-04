994233a9f3e4e0c2b08ff8008e263d07e72ded9c
Alessandro dos Santos <alesantos6789@gmail.com>
2026-07-03 21:40:04 -0300

    atualiza docs versão 2

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -80,6 +80,7 @@ async function request<T>(
 
   const headers: HeadersInit = {
     "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true",
     ...(token ? { Authorization: `Bearer ${token}` } : {}),
     ...options.headers,
   };
